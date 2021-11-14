3f4d929b494e5926c9f14a914be26e459403919c
Ramon Lino <pereira.ramonlino@gmail.com>
2021-11-14 13:41:34 -0300

    Comparando todas as base de dados para obitos

--- data/exportando_dados.py
+++ data/exportando_dados.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
-dados_ = pd.read_csv("data\\dados_federais_vacinacao_cg.csv", encoding='UTF-8', error_bad_lines=False)
+dados = pd.read_csv("data\\dados_federais_obitos_cg.csv", encoding='UTF-8', error_bad_lines=False)
 
-#dados_ = dados_.loc[dados_['vacina_dataaplicacao'] >= '2021-06-01']
-dados_.drop(
-    ['paciente_endereco_coibgemunicipio',
-     'paciente_endereco_copais',
-     'paciente_endereco_nmpais',
-     'paciente_endereco_uf',
-     'paciente_nacionalidade_enumnacionalidade',
-     'estabelecimento_valor',
-     'estabelecimento_municipio_codigo',
-     'estabelecimento_uf',
-     'estabelecimento_municipio_nome',
-     'vacina_fabricante_referencia'], 
-     inplace=True, axis=1)
-dados_.to_csv('dados_federais_vacinacao_cg.csv', index=False)
+dados["dataNotificacao"] = dados["dataNotificacao"].str.slice(stop=10)
+dados = dados.loc[dados['dataNotificacao'] >= '2021-06-01']
+dados = dados.loc[dados['dataNotificacao'] <= '2021-08-31']
+dados.to_csv('dados_federais_obitos_cg.csv', index=False)
+
+
+
+#dados = dados_.loc[dados_['vacina_dataaplicacao'] >= '2021-06-01']
+# dados.drop(
+#     ['paciente_endereco_coibgemunicipio',
+#      'paciente_endereco_copais',
+#      'paciente_endereco_nmpais',
+#      'paciente_endereco_uf',
+#      'paciente_nacionalidade_enumnacionalidade',
+#      'estabelecimento_valor',
+#      'estabelecimento_municipio_codigo',
+#      'estabelecimento_uf',
+#      'estabelecimento_municipio_nome',
+#      'vacina_fabricante_referencia'], 
+#      inplace=True, axis=1)
